f6e19b5db655836716213e68bb5a19f91ac30d19
diegobarr <d1390xd.v@gmail.com>
2025-07-24 11:55:23 -0600

    Final función 1

--- menuOperaciones.py
+++ menuOperaciones.py
@@ -1,18 +1,16 @@
 def operar_nums(cant_nums):
     nums_sum = 0
-    cont=0
+    count_pst=0
+    count_ngt=0
     for i in range(cant_nums):
         num=int(input("Ingrese un numero: "))
         nums_sum+=num
+        if num<0:
+            count_pst+=1
+        else:
+            count_ngt+=1
+    print(f"La suma los {cant_nums} números es {nums_sum}, el promedio es {nums_sum/cant_nums}, existen {count_pst} numeros por positivos y {count_ngt} negativos")
 
-    print(f"La suma los {cant_nums} números es {nums_sum}, el promedio es {nums_sum/cant_nums}, ")
-
-#def promediar_nums(cant_nums):
- #   avrg_nums=[]
-  #  for i in cant_nums:
-   #     print("Ingrese un numero: ")
-    #    avrg_nums.append(i)
-   # return avrg_nums
 
 nums=int(input("Ingrese la cantidad de numeros: "))
 
